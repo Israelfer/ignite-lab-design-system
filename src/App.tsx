a4aaeee519f2312b5ddc3d36b5e381777df85b2c
Israel Batista <israel.batista@ab-inbev.com>
2022-10-14 00:04:18 -0300

    update npmrc

--- src/App.tsx
+++ src/App.tsx
@@ -54,10 +54,10 @@ export function App() {
 
       <footer className="flex flex-col items-center gap-4 mt-8">
         <Text asChild size="sm">
-          <a href="#" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha</a>
+          <a href="" className="text-gray-400 underline hover:text-gray-200">Esqueceu sua senha</a>
         </Text>
         <Text asChild size="sm">
-          <a href="#" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Crie uma agora!</a>
+          <a href="" className="text-gray-400 underline hover:text-gray-200">Não possui conta? Crie uma agora!</a>
         </Text>
 
       </footer>
